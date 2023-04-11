1a69ed557ac87f05beacee12905b6a332e861d6f
GhostITShell <onlygreyhat@gmail.com>
2023-04-11 21:58:35 +0300

    Workspace Created!

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+const blog = { id: 1, title: "Blog title", description: "Blog description" };
+app.get("/", (req, res) => {
+  res.send(blog);
+});
+
+const port = 3001;
+app.listen(port, () => {
+  console.log(`Sayfanız ${port} portunda çalışıyor.`);
+});
